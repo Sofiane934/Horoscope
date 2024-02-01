49939d53786e89560b0ab3679e8d51b9e6ab1f26
Sofiane934 <157029439+Sofiane934@users.noreply.github.com>
2024-02-01 16:30:48 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+   <form action="index.php?action=connexion" method="GET">
+    <label for="login">login</label>
+    <input type="text" id="login" name="login">
+    <label for="mdp">mot de passe</label>
+    <input type="text" id="mdp" name="mdp">
+    <input type="submit" value="Valider">
+
+</form> 
+</body>
+</html>
