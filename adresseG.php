79aee0d0e6fb0c02562777292e96a8d6b454a34d
Sofiane934 <157029439+Sofiane934@users.noreply.github.com>
2024-02-08 16:26:41 +0100

    Delete adresseG.php

--- adresseG.php
+++
@@ -1,8 +0,0 @@
-<form method="GET" action="passageRecupG.php">
-<input type="hidden" name="nom" value="<?=$nom?>">
-<input type="hidden" name="prenom" value="<?=$prenom?>">
-<input type="hidden" name="age" value="<?=$age?>">
-<label for="adresse">adresse</label>
-<input type="text" id="adresse" name="adresse"> <br>
-<input type="submit" value="Valider">
-</form>
